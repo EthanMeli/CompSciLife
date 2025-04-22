2976ba394820765f44085756410bf029038460f8
John <40719502+koreantrash@users.noreply.github.com>
2025-04-22 16:00:13 -0600

    Update tiles.h

--- tiles.h
+++ tiles.h
@@ -8,32 +8,51 @@ enum class TileType {
     MONEY,
     POWERUP,
     FORWARD,
-    BACKWARD
+    BACKWARD,
+    LIFE_EVENT
 };
 
 class Tile {
-    public:
-        virtual ~Tile() {}
-        //activation method so that player gets affected by the tile
+public:
+    virtual ~Tile() {}
+    virtual void activate(Player& player) = 0;
 };
 
-// Derived Tiles
+// Tile Types
+class EmptyTile : public Tile {
+public:
+    void activate(Player& player) override;
+};
+
 class MoneyTile : public Tile {
+public:
+    void activate(Player& player) override;
 };
 
 class PowerupTile : public Tile {
+public:
+    void activate(Player& player) override;
 };
 
 class MoveForwardTile : public Tile {
+public:
+    void activate(Player& player) override;
 };
 
 class MoveBackwardTile : public Tile {
+public:
+    void activate(Player& player) override;
+};
+
+class LifeEventTile : public Tile {
+public:
+    void activate(Player& player) override;
 };
 
 // Factory
 class TileFactory {
-    public:
-        static Tile* createTile(TileType type);
+public:
+    static Tile* createTile(TileType type);
 };
 
 #endif // TILES_H
